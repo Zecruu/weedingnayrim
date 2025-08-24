2b65be6dddf0b9b784b761acbbe7562b605f5c5c
DemchakMichael-FS <your-email@example.com>
2025-08-24 19:10:24 -0400

    Font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ export default {
       fontFamily: {
         'serif': ['Playfair Display', 'serif'],
         'sans': ['Lato', 'sans-serif'],
-        'script': ['Dancing Script', 'cursive'],
+        'script': ['Playfair Display', 'serif'],
       },
     },
   },
